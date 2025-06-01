0dfdf101c534602f6272b19a6029a9102d74ad33
Max Base <MaxBaseCode@gmail.com>
2025-06-01 23:24:10 +0330

    audio added, loading added, back functionality changed.

--- app/src/main/java/ir/kashan/raziazriazy/MainActivity.java
+++ app/src/main/java/ir/kashan/raziazriazy/MainActivity.java
@@ -12,69 +12,78 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PAGE_STARTED = 0x1;
     private int webViewPreviousState;
-    private final int PAGE_STARTED = 0x1;
     private CoordinatorLayout rootView;
     private WebView webView;
+    private Dialog loadingDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webView);
-        rootView = (CoordinatorLayout) findViewById(R.id.root_view);
+
+        rootView = findViewById(R.id.root_view);
+        webView = findViewById(R.id.webView);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
+            if ((getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
                 WebView.setWebContentsDebuggingEnabled(true);
             }
         }
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+        webSettings.setGeolocationDatabasePath(getFilesDir().getPath());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
         webView.setInitialScale(1);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setUseWideViewPort(true);
         webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         webView.setScrollbarFadingEnabled(false);
-        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webView.getSettings().setBuiltInZoomControls(false);
         webView.setWebViewClient(new GeoWebViewClient());
-        webView.getSettings().setJavaScriptEnabled(true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-        webView.getSettings().setGeolocationEnabled(true);
         webView.setWebChromeClient(new GeoWebChromeClient());
-        webView.getSettings().setDatabaseEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setGeolocationDatabasePath(getFilesDir().getPath());
+
         webView.loadUrl("https://raziazriazy.ir/");
     }
 
-    public class GeoWebChromeClient extends android.webkit.WebChromeClient {
+    public class GeoWebChromeClient extends WebChromeClient {
         @Override
-        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
+        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
             callback.invoke(origin, true, false);
         }
     }
 
     public class GeoWebViewClient extends WebViewClient {
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url = request.getUrl().toString();
             if (url.endsWith(".pdf")) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setDataAndType(android.net.Uri.parse(url), "application/pdf");
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-
                 try {
                     startActivity(intent);
                 } catch (android.content.ActivityNotFoundException e) {
@@ -86,44 +95,48 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
 
-        Dialog loadingDialog = new Dialog(MainActivity.this);
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
             webViewPreviousState = PAGE_STARTED;
+            if (loadingDialog == null) {
+                loadingDialog = new Dialog(MainActivity.this);
+                loadingDialog.setContentView(R.layout.loading_spinner);
+                loadingDialog.setCancelable(false);
+                loadingDialog.show();
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (webViewPreviousState == PAGE_STARTED && loadingDialog != null) {
+                loadingDialog.dismiss();
+                loadingDialog = null;
+            }
         }
 
         @Override
         public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
             if (!isConnected()) {
-                final Snackbar snackBar = Snackbar.make(rootView, "No Internet Connection ", Snackbar.LENGTH_INDEFINITE);
+                final Snackbar snackBar = Snackbar.make(rootView, "No Internet Connection", Snackbar.LENGTH_INDEFINITE);
                 snackBar.setAction("Enable Data", new View.OnClickListener() {
                     @Override
-                    public void onClick(View view) {
+                    public void onClick(View v) {
                         startActivityForResult(new Intent(Settings.ACTION_WIRELESS_SETTINGS), 0);
-                        webView.loadUrl("javascript:window.location.reload( true )");
+                        webView.reload();
                         snackBar.dismiss();
                     }
                 });
                 snackBar.show();
             }
-            super.onReceivedError(view, request, error);
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            if (webViewPreviousState == PAGE_STARTED) {
-                if (null != loadingDialog) {
-                    loadingDialog.dismiss();
-                    loadingDialog = null;
-                }
-            }
         }
     }
 
     public boolean isConnected() {
-        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (null != cm) {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
             NetworkInfo info = cm.getActiveNetworkInfo();
             return (info != null && info.isConnected());
         }
@@ -131,7 +144,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-	public void onBackPressed() {
-        super.onBackPressed();
-	}
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
